58f6637c6c55e2047c1c83284af45a66fe6e2d1a
csal90 <35342933+csal90@users.noreply.github.com>
2020-01-13 21:52:14 -0600

    week two

---
+++ week1/main.py
@@ -0,0 +1,49 @@
+# exercise 1:
+# - 0 1 2 3 4
+# for nums in range(0,5):
+#     print(nums)
+
+# - 1 3 5 7
+# for nums in range(1,8,2):
+#     print(nums)
+
+# -1
+# for nums in range(1,2):
+#     print(nums)
+
+# - 0 3
+# for nums in range(1,2):
+#     print(nums)
+#
+# - 0 2 4 6 8 10 12
+# for nums in range(0, 4, 3):
+#     print(nums)
+#
+# - 17 13 9 5
+# for nums in range(0, 13, 2):
+#     print(nums)
+
+# exercise 2:
+# Write a function evenNums(), which prompts a message "Enter a whole number: " to the user to get a number n and prints out even numbers from 0 until n
+
+# def evenNums():
+#     n = int(input('Enter a whole number: '))
+#     for num in range(0, n+1, 2):
+#         print(num, end=' ')
+# evenNums()
+
+# printNums(str) prints out even numbers if str == 'even' or odd numbers if str == 'odd'.
+# The function prompt a message "Enter a whole number: ", get a number n from the user then print out either even numbers or odd numbers until n
+
+def printNums(str):
+    n = int(input('Enter a whole number: '))
+
+    if str == 'even':
+        for num in range(0, n + 1, 2):
+            print(num, end=' ')
+    elif str == 'odd':
+        for num in range(0, n + 2, 2):
+            print(num, end=' ')
+
+
+printNums('odd')
