7ec90b9e53b67881ef8547d8fbd8b6d718120f9f
csal90 <35342933+csal90@users.noreply.github.com>
2020-01-22 19:53:23 -0600

    added files

---
+++ week1/lecture0122.py
@@ -0,0 +1,40 @@
+# Implement a function product(l) that takes a list l as parameter and multiplies all the numbers
+# in l, and returns the product of the multiplication
+def product(l):
+    result = 1
+    for num in l:
+        result = result * num
+    print(result)
+
+
+# Write a function that concatenates all #s in a string to 1 string
+def digits(s):
+    r = ''
+    for c in s:
+        if c.isdigit():
+            r = r + c
+    return r
+
+
+# Write a function divisors(n) that takes an integer n and returns a list of the positive divisors of
+# n
+# divisors(8)
+# [1,2,4,8]
+def divisors(n):
+    l = []
+    for num in range(l, n + 1):
+        if n % num == 0:
+            l.append(num)
+    return l
+
+# Counter loop
+l = [1, 3, 5, 7, 9]
+for in range(0, len(l)):
+    print(l[i])
+
+# Counter loop - Write a function ascending(l) to check whether a list of numbers is sorted in order
+def ascending(l):
+    for i in range(len(l)-1):
+        if l[i]>l[i]:
+            return False
+    return True
--- week1/main.py
+++ week1/main.py
@@ -1,89 +1,85 @@
-# exercise 1:
-# - 0 1 2 3 4
-# for nums in range(0,5):
-#     print(nums)
+#exercise 1:
+- 0 1 2 3 4
+for nums in range(0,5):
+    print(nums)
 
-# - 1 3 5 7
-# for nums in range(1,8,2):
-#     print(nums)
+- 1 3 5 7
+for nums in range(1,8,2):
+    print(nums)
 
-# -1
-# for nums in range(1,2):
-#     print(nums)
+-1
+for nums in range(1,2):
+    print(nums)
 
-# - 0 3
-# for nums in range(1,2):
-#     print(nums)
-#
-# - 0 2 4 6 8 10 12
-# for nums in range(0, 4, 3):
-#     print(nums)
-#
-# - 17 13 9 5
-# for nums in range(0, 13, 2):
-#     print(nums)
+- 0 3
+for nums in range(1,2):
+    print(nums)
 
-# exercise 2:
-# Write a function evenNums(), which prompts a message "Enter a whole number: " to the user to get a number n and prints out even numbers from 0 until n
+- 0 2 4 6 8 10 12
+for nums in range(0, 4, 3):
+    print(nums)
 
-# def evenNums():
-#     n = int(input('Enter a whole number: '))
-#     for num in range(0, n+1, 2):
-#         print(num, end=' ')
-# evenNums()
+- 17 13 9 5
+for nums in range(0, 13, 2):
+    print(nums)
 
-# printNums(str) prints out even numbers if str == 'even' or odd numbers if str == 'odd'.
-# The function prompt a message "Enter a whole number: ", get a number n from the user then print out either even numbers or odd numbers until n
+#exercise 2:
+#Write a function evenNums(), which prompts a message "Enter a whole number: " to the user to get a number n and prints out even numbers from 0 until n
 
-# def printNums(str):
-#     n = int(input('Enter a whole number: '))
-#
-#     if str == 'even':
-#         for num in range(0, n + 1, 2):
-#             print(num, end=' ')
-#     elif str == 'odd':
-#         for num in range(0, n + 2, 2):
-#             print(num, end=' ')
-#
-#
-# printNums('')
+def evenNums():
+    n = int(input('Enter a whole number: '))
+    for num in range(0, n+1, 2):
+        print(num, end=' ')
+evenNums()
 
-# #Write statements to do the following:
-# # str='This will be a sunny day.'
-# # 1. assign a variable n to the count of 'day' in str
-# str = 'This will be a sunny day'
-# n = str.count('day')
-# print(n)
-# # 2. assign a variable pos to the location/index of string 'sunny' in str
-# pos = str.find('sunny')
-# print(pos)
-# # 3. replace 'sunny' with 'cloudy'
-# str.replace('sunny', 'cloudy')
-# print(str)
+#printNums(str) prints out even numbers if str == 'even' or odd numbers if str == 'odd'.
+#The function prompt a message "Enter a whole number: ", get a number n from the user then print out either even numbers or odd numbers until n
 
-# login(l) takes a list l consists of strings or names, prompts a message to 'Enter your login id: ' to the user and get a login id from the user, then it check whether the login id is within the list l
-# def login(l):
-#     loginId = input('Enter your login id: ')
-#     if loginId in l:
-#         print('login successfully')
-#     else:
-#         print('invalid login id')
-#
-#
-# login(['chris', 'nick', 'emily'])
+def printNums(str):
+    n = int(input('Enter a whole number: '))
 
-# checkTemp(temp) takes integer parameter temp, and prints a message depending the value of temp
+    if str == 'even':
+        for num in range(0, n + 1, 2):
+            print(num, end=' ')
+    elif str == 'odd':
+        for num in range(0, n + 2, 2):
+            print(num, end=' ')
+
+
+printNums('')
+
+#Write statements to do the following:
+# str='This will be a sunny day.'
+# 1. assign a variable n to the count of 'day' in str
+str = 'This will be a sunny day'
+n = str.count('day')
+print(n)
+# 2. assign a variable pos to the location/index of string 'sunny' in str
+pos = str.find('sunny')
+print(pos)
+# 3. replace 'sunny' with 'cloudy'
+str.replace('sunny', 'cloudy')
+print(str)
+
+#login(l) takes a list l consists of strings or names, prompts a message to 'Enter your login id: ' to the user and get a login id from the user, then it check whether the login id is within the list l
+def login(l):
+    loginId = input('Enter your login id: ')
+    if loginId in l:
+        print('login successfully')
+    else:
+        print('invalid login id')
+
+#checkTemp(temp) takes integer parameter temp, and prints a message depending the value of temp
 # prints 'it's hot' when temp > 86
 # prints 'It's cool' when temp is > 0 and temp <= 86
 # prints "It's freezing" when temp <= 0
 
-# def checkTemp(temp):
-#     if temp > 86:
-#         print("It's hot")
-#     elif 0 < temp <= 86:
-#         print("It's cool")
-#     else:
-#         print("It's freezing")
-#
-#
-# checkTemp(12)
+def checkTemp(temp):
+    if temp > 86:
+        print("It's hot")
+    elif 0 < temp <= 86:
+        print("It's cool")
+    else:
+        print("It's freezing")
+
+checkTemp()
